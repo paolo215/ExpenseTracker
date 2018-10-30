d18323308dc4ab224e79e8c1085a9074448dad9a
Paolo Villanueva <paolo_villanueva215@yahoo.com>
2018-10-30 00:57:35 -0400

    Added initial nav menu

---
+++ public/directives/main-nav.js
@@ -0,0 +1,14 @@
+"use strict";
+
+app.directive("mainNav", function() {
+    return {
+        restrict: "E",
+        scope: {},
+        controller: ["$scope", "$http", function mainController($scope, $http) {
+
+
+     }],
+    templateUrl: "./templates/main-nav.html"
+
+    }
+});
