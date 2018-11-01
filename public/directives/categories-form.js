dcf033012eef232817cbcdb02b7d4433dd1be262
Paolo Villanueva <paolo_villanueva215@yahoo.com>
2018-11-01 00:28:31 -0400

    Updated GUI to include some client side validation

--- public/directives/categories-form.js
+++ public/directives/categories-form.js
@@ -7,8 +7,13 @@ app.directive("categoriesForm", function() {
         scope: {},
         controller: ["$scope", "$http", function mainController($scope, $http) {
 
-            $scope.showForm = false;
             $scope.showForm = true;
+            $scope.formData = {};
+            
+
+            $scope.clearForm = function() {
+                $scope.formData = {};
+            };
 
             $scope.submit = function() {
                 console.log("submitting");
@@ -16,11 +21,15 @@ app.directive("categoriesForm", function() {
                 $http.post("/api/categories/addCategory", $scope.formData)
                     .then(function(success) {
                     console.log(success);
+
+                    $scope.clearForm();
                 }, function(err) {
             
                 });
                 // clear form
             };
+
+            $scope.clearForm();
 
      }],
     templateUrl: "./templates/categories-form.html"
